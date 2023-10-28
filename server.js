4ef56fb9a88ac10ad1a8882ee5325d2c85c6a819
Lokendra Bhat <lokendrabhat.2022@gmail.com>
2023-10-28 18:46:00 +0545

    routes folder added

--- server.js
+++ server.js
@@ -6,6 +6,16 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+
+//// Its a first try to check the request and response without routes using get 
+// app.get("/api/contacts", (req, res) => {
+//     // res.send("Get all your msg")
+//     // res.json({ message: "Get all your msg"});
+//     res.status(200).json({msg: "Get all of Messages"});
+// });
+
+app.use("/api/contacts", require("./routes/contactRoutes"));     // Middleware
+
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
