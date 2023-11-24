d7b2821a1d766cf5d87f55ce0c6bf1969e0f4ae4
Lokendra Bhat <lokendrabhat.2022@gmail.com>
2023-11-24 15:05:05 +0545

    successfull changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,26 +1,44 @@
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcrypt");
+const User = require("../models/userModel");
 
 // @desc Register a user
-// @route GET /api/user/register
+// @route POST /api/users/register
 // @access public
 const registerUser = asyncHandler(async (req, res) => {
+    const { username, email, password } = req.body;
+    if (!username || !email || !password) {
+        res.status(400);
+        throw new Error("All fields are mendatory!");
+    }
+    const userAvailable = await User.findOne({ email });
+    if (userAvailable) {
+        res.status(400);
+        throw new Error("User already registered")
+    }
+
+    console.log("step2");
+    // Hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("Hashed Password: ", hashedPassword);
+
     res.json({ message: "Register the user" });
 });
 
 
 // @desc User Login
-// @route GET /api/user/login
+// @route POST /api/users/login
 // @access public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login user" })
+    res.json({ message: "Login user" });
 });
 
 
 // @desc Current user info
-// @route GET /api/user/current
+// @route POST /api/users/current
 // @access private
 const currentUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Current user info" })
+    res.json({ message: "Current user info" });
 });
 
 
--- models/userModel.js
+++ models/userModel.js
@@ -15,7 +15,10 @@ const userSchema = mongoose.Schema({
         type: String,
         required: [true, "Please add the password"]
     }
+    
 }, {
     timestamps: true,
 }
 );
+
+module.exports = mongoose.model("User", userSchema);
